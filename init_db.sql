05276d0cb11c071fa8f1841096c86fb6cdfd59c9
Wassim Benzarti <wass11121996@gmail.com>
2020-10-20 10:00:08 +0000

    Add ngrok and sql

---
+++ init_db.sql
@@ -0,0 +1,24 @@
+-- Adminer 4.7.7 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int NOT NULL,
+  `firstname` varchar(40) NOT NULL,
+  `lastname` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `user` (`id`, `firstname`, `lastname`) VALUES
+(1,	'Ahmed',	'Ramzi'),
+(2,	'Souad',	'Mezghenni'),
+(3,	'Mourad',	'Lahwel'),
+(4,	'Monia',	'Landolsi');
+
+-- 2020-10-20 09:59:23
--- ngrok_serve.sh
+++ ngrok_serve.sh
@@ -1,1 +1,2 @@
+cat ~/.ssh/id_rsa.pub
 ssh -R 0:localhost:3306 tunnel.eu.ngrok.com tcp 3306
